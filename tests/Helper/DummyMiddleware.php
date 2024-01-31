f0d17b6038c0b05d0e5e63ce2ccd4c416e89cade
Tom Rune Flo <tom@upnode.no>
2024-01-31 23:52:14 +0100

    add more basic tests

--- tests/Helper/DummyMiddleware.php
+++ tests/Helper/DummyMiddleware.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Nanofraim\Test\Helper;
 
-class DummyMiddleware extends \Nanofraim\Http\AbstractMiddleware
+use Nanofraim\Http\AbstractMiddleware;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class DummyMiddleware extends AbstractMiddleware
 {
-    public function process(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Server\RequestHandlerInterface $handler): \Psr\Http\Message\ResponseInterface
-    {
-        return $this->responseFactory->createResponse(200);
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler
+    ): ResponseInterface {
+        $response = $this->responseFactory->createResponse(200);
+        $response = $response->withHeader('X-Dummy-Middleware', 'true');
+
+        $body = $response->getBody();
+        $body->write('Hello World');
+
+        return $response->withBody($body);
     }
 }
--- tests/NanofraimTest.php
+++ tests/NanofraimTest.php
@@ -4,35 +4,143 @@ declare(strict_types=1);
 
 namespace Nanofraim\Test;
 
+use Nanofraim\Application;
 use Nanofraim\Http\ResponseEmitter;
+use Nanofraim\Http\ResponseFactory;
+use Nanofraim\Test\Helper\DummyMiddleware;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Tomrf\Autowire\Autowire;
+use Tomrf\ConfigContainer\ConfigContainer;
+use Tomrf\ServiceContainer\ServiceContainer;
 
 /**
  * @internal
+ *
  * @covers \Nanofraim\Application
+ * @covers \Nanofraim\Http\AbstractMiddleware
+ * @covers \Nanofraim\Http\ResponseEmitter
+ * @covers \Nanofraim\Http\ResponseFactory
+ * @covers \Nanofraim\RelayMiddlewareResolver
+ * @covers \Nanofraim\Test\Helper\DummyMiddleware
  */
 final class NanofraimTest extends TestCase
 {
+    private static Application $app;
+
     public static function setUpBeforeClass(): void
     {
-        // ...
+        self::$app = self::createApplication();
     }
 
     public function testInstantiationOfApplication(): void
     {
-        static::assertInstanceOf(
-            \Nanofraim\Application::class,
-            new \Nanofraim\Application(
-                new \Tomrf\ServiceContainer\ServiceContainer(
-                    new \Tomrf\Autowire\Autowire()
-                ),
-                new \Tomrf\ConfigContainer\ConfigContainer([
-                    'middleware' => [
-                        \Nanofraim\Test\Helper\DummyMiddleware::class,
-                    ],
-                ]),
-                new ResponseEmitter(),
-            )
+        $this->assertInstanceOf(
+            Application::class,
+            self::$app
+        );
+    }
+
+    public function testAppConstructor(): void
+    {
+        $serviceContainer = new ServiceContainer(
+            new Autowire()
+        );
+
+        $serviceContainer->add(
+            DummyMiddleware::class,
+            static fn () => new DummyMiddleware(new ResponseFactory())
+        );
+
+        $app = new Application(
+            $serviceContainer,
+            new ConfigContainer([
+                'middleware' => [
+                    DummyMiddleware::class,
+                ],
+            ]),
+            new ResponseEmitter(),
+        );
+
+        $this->assertInstanceOf(
+            Application::class,
+            $app
+        );
+    }
+
+    public function testAppCreateServerRequestFromGlobals(): void
+    {
+        $request = self::$app->createServerRequestFromGlobals();
+
+        $this->assertInstanceOf(
+            ServerRequestInterface::class,
+            $request
+        );
+    }
+
+    public function testAppHandle(): void
+    {
+        $request = self::$app->createServerRequestFromGlobals();
+
+        $response = self::$app->handle($request);
+
+        $this->assertInstanceOf(
+            ResponseInterface::class,
+            $response
+        );
+
+        // verify http code 200
+        $this->assertSame(
+            200,
+            $response->getStatusCode()
+        );
+
+        // verify X-Dummy-Middleware header
+        $this->assertSame(
+            'true',
+            $response->getHeaderLine('X-Dummy-Middleware')
+        );
+
+        // verify body
+        $this->assertSame(
+            'Hello World',
+            (string) $response->getBody()
+        );
+    }
+
+    public function testAppEmit(): void
+    {
+        $this->expectsOutput();
+
+        $request = self::$app->createServerRequestFromGlobals();
+        $response = self::$app->handle($request);
+
+        self::$app->emit($response);
+
+        // check body output
+        $this->expectOutputRegex('/Hello World/');
+    }
+
+    private static function createApplication(): Application
+    {
+        $serviceContainer = new ServiceContainer(
+            new Autowire()
+        );
+
+        $serviceContainer->add(
+            DummyMiddleware::class,
+            static fn () => new DummyMiddleware(new ResponseFactory())
+        );
+
+        return new Application(
+            $serviceContainer,
+            new ConfigContainer([
+                'middleware' => [
+                    DummyMiddleware::class,
+                ],
+            ]),
+            new ResponseEmitter(),
         );
     }
 }
