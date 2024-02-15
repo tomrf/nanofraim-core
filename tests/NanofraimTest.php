15f23ebeb160bca6ebdc9337c4ee89b37e44caac
Tom Rune Flo <tom@upnode.no>
2024-02-15 02:14:30 +0100

    more tests, 90% coverage (classes)

--- tests/NanofraimTest.php
+++ tests/NanofraimTest.php
@@ -19,10 +19,17 @@ use Tomrf\ServiceContainer\ServiceContainer;
 /**
  * @internal
  *
- * @covers \Nanofraim\Application
+ * @covers \Nanofraim\Exception\FrameworkException
  * @covers \Nanofraim\Http\AbstractMiddleware
+ * @covers \Nanofraim\Http\AbstractController
  * @covers \Nanofraim\Http\ResponseEmitter
  * @covers \Nanofraim\Http\ResponseFactory
+ * @covers \Nanofraim\Provider\LoggerProvider
+ * @covers \Nanofraim\Provider\ResponseFactoryProvider
+ * @covers \Nanofraim\Provider\SessionProvider
+ * @covers \Nanofraim\Provider\SimpleCacheProvider
+ * @covers \Nanofraim\Application
+ * @covers \Nanofraim\AbstractProvider
  * @covers \Nanofraim\RelayMiddlewareResolver
  * @covers \Nanofraim\Test\Helper\DummyMiddleware
  */
@@ -199,5 +206,124 @@ final class NanofraimTest extends TestCase
         $request = $app->createServerRequestFromGlobals();
 
         $app->handle($request);
+    }
+
+    public function testAbstractController(): void
+    {
+        $controller = new class (
+            self::$app->createServerRequestFromGlobals(),
+            new ResponseFactory()
+        ) extends \Nanofraim\Http\AbstractController {
+            public function __invoke(): ResponseInterface
+            {
+                return $this->responseFactory->createResponse(200, 'Hello World');
+            }
+        };
+
+        $response = $controller();
+
+        self::assertInstanceOf(
+            ResponseInterface::class,
+            $response
+        );
+
+        // verify http code 200
+        self::assertSame(
+            200,
+            $response->getStatusCode()
+        );
+    }
+
+    public function testResponseEmitter(): void
+    {
+        $request = self::$app->createServerRequestFromGlobals()->withAttribute('X-Dummy-Middleware', true);
+        $response = self::$app->handle($request);
+
+        self::$app->emit($response);
+
+        // check body output
+        $this->expectOutputRegex('/X-Dummy-Middleware: true/');
+    }
+
+    public function testProviderLoggerProvider(): void
+    {
+        $tempLogFile = tempnam(sys_get_temp_dir(), '__NanofraimTest_');
+
+        $loggerProvider = new \Nanofraim\Provider\LoggerProvider(
+            new ConfigContainer([
+                'path' => $tempLogFile,
+            ]),
+            new ResponseFactory()
+        );
+
+        $logger = $loggerProvider->createService();
+
+        $logger->info('Hello World');
+
+        self::assertStringContainsString(
+            'Hello World',
+            file_get_contents($tempLogFile)
+        );
+
+        unlink($tempLogFile);
+    }
+
+    public function testProviderSessionProvider(): void
+    {
+        $sessionProvider = new \Nanofraim\Provider\SessionProvider(
+            new ConfigContainer([
+                'name' => 'NanofraimTest',
+            ]),
+            new ResponseFactory()
+        );
+
+        $session = $sessionProvider->createService();
+
+        $session->set('foo', 'bar');
+
+        self::assertSame(
+            'bar',
+            $session->get('foo')
+        );
+    }
+
+    public function testProviderResponseFactoryProvider(): void
+    {
+        $responseFactoryProvider = new \Nanofraim\Provider\ResponseFactoryProvider(
+            new ConfigContainer(),
+        );
+
+        $responseFactory = $responseFactoryProvider->createService();
+
+        $response = $responseFactory->createResponse(123, 'Test');
+
+        self::assertInstanceOf(
+            ResponseInterface::class,
+            $response
+        );
+
+        self::assertSame(
+            123,
+            $response->getStatusCode()
+        );
+    }
+
+
+    public function testProviderSimpleCacheProvider(): void
+    {
+        $simpleCacheProvider = new \Nanofraim\Provider\SimpleCacheProvider(
+            new ConfigContainer([
+                'adapter' => 'array'
+            ]),
+        );
+
+        $simpleCache = $simpleCacheProvider->createService();
+
+        $simpleCache->set('foo', 'bar');
+
+        self::assertSame(
+            'bar',
+            $simpleCache->get('foo')
+        );
     }
 }
